b91957225ca222f1f5016421bb5d35fa2c292bcb
ofir <ofirmeidan1998@gmail.com>
2022-06-02 13:09:35 +0300

    adding tests

--- test.c
+++ test.c
@@ -6,20 +6,82 @@ int main(int argc, char const *argv[])
 {
     printf("cerating..\n");
     mymkfs(100000); 
-    myopendir("root/test");
-    myopendir("root/test3");
-    int fd = myopen("root/test/newfile", NULL);
-    int fd2 = myopen("root/test/newfil1", NULL);
-    printdir("root");
-    printdir("test");
-    printdir("test3");
+    int t1 = myopendir("root/test1");
+    int t2 = myopendir("root/test1");
 
-    char* test = "this is a test"; 
-    int ptr =  mywrite(fd, test, 25);
+    int t = (t1 == t2);
+    if(t == 1)
+    {
+        printf("test myopendir PASSED\n");
+    }else{
+        printf("test myopendir FAILED\n");
+    }
+
+
+    myopendir("root/test2");
+    int fd = myopen("root/test1/file1", 0);
+    int testfd = myopen("root/test1/file1", 0);
+    int fd2 = myopen("root/test2/file2",0);
+    int fd3 = myopen("root/test1/file2",0);
+  
+    int i = (fd == testfd);
+    if(i == 1)
+    {
+        printf("myopen test PASSED\n");
+    }else{
+        printf("myopen test FAILED\n");
+    }
+    testfd = myclose(testfd);
+
+    i = (fd != testfd);
+    if(i == 1)
+    {
+        printf("myclose test PASSED\n");
+    }else{
+        printf("myclose test FAILED\n");
+    }
+
+    i = (fd2 == fd3);
+    if(i == 1)
+    {
+        printf("different directory open file PASSED\n");
+    }else{
+        printf("different directory open file FAILED\n");
+    }
+
+    char* test = "this is a test\n"; 
+
+    int ptr =  mywrite(fd2, test, 25);
     char buf[25];
-    mylseek(fd, -25, SEEK_CUR);
-    myread(fd, buf, 25);
-    printf("%s", buf);
+    char buf2[25];
+    mylseek(fd2, -25, SEEK_CUR);
+    myread(fd2, buf, 25);
+
+
+     if(strcmp(buf, test) ==0)
+    {
+        printf("read test PASSED\n");
+    }else
+    {
+        printf("read test FAILED\n");
+    }
+
+    mylseek(fd2,-15,SEEK_CUR);
+    myread(fd2, buf2, 15);
+   
+    if(strcmp(buf2, "test\n") ==0)
+    {
+        printf("seek test PASSED\n\n");
+    }else
+    {
+        printf("seek test FAILED\n\n");
+    }
+
+    printf("our fs looks like this:\n\n");
+    printdir("root");
+    printdir("test1");
+    printdir("test2");
+
     myclose(fd);
     return 0;
 }
