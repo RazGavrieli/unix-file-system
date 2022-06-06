e842dd92318b52524c8c61593a5ffefb9098a0e9
Raz <razgavrieli@gmail.com>
2022-06-06 22:22:57 +0300

    small fix

--- sources/mystdio.c
+++ sources/mystdio.c
@@ -178,32 +178,6 @@ int myfscanf(myFILE * stream, const char * format, ...) {
     return 1;
 }
 
-void reverse(char s[])
- {
-     int i, j;
-     char c;
-
-     for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
-         c = s[i];
-         s[i] = s[j];
-         s[j] = c;
-     }
-} 
- void itoa(int n, char s[])
- {
-     int i, sign;
-
-     if ((sign = n) < 0)  /* record sign */
-         n = -n;          /* make n positive */
-     i = 0;
-     do {       /* generate digits in reverse order */
-         s[i++] = n % 10 + '0';   /* get next digit */
-     } while ((n /= 10) > 0);     /* delete it */
-     if (sign < 0)
-         s[i++] = '-';
-     s[i] = '\0';
-     reverse(s);
-}  
 
 int myfprintf(myFILE * stream, const char * format, ...) {
     /**
@@ -220,7 +194,7 @@ int myfprintf(myFILE * stream, const char * format, ...) {
         if (format[i]=='%') {
             if (format[i+1]=='d') {
                 int currvar = va_arg ( arguments, int );
-                itoa(currvar, currbuffer);
+                sprintf(currbuffer,"%d",currvar);
             } else if (format[i+1]=='f') {
                 double currvar = va_arg ( arguments, double );
                 sprintf(currbuffer,"%f",currvar);
